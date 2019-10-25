9dbb8095a5d4c3371630662d0b407d1daa9525f5
Jose Velarde <jvicius@gmail.com>
2019-10-25 12:20:46 -0500

    Memento Pattern

---
+++ Memento/Models/Originator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Memento.Models
+{
+    public class Originator
+    {
+        // For the sake of simplicity, the originator's state is stored inside a
+        // single variable.
+        private string _state;
+
+        public Originator(string state)
+        {
+            this._state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        public void SetState(string value)
+        {
+            this._state = value;
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+
+        public string GetState()
+        {
+            Console.WriteLine($"Originator: My Current state is: {_state}");
+            return this._state;
+        }
+
+        public IMemento Save()
+        {
+            return new ConcreteMemento(this._state);
+        }
+
+        // Restores the Originator's state from a memento object.
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is ConcreteMemento))
+            {
+                throw new Exception("Unknown memento class " + memento.ToString());
+            }
+
+            this._state = memento.GetState();
+            Console.Write($"Originator: My state has changed to: {_state}");
+        }
+
+    }
+}
--- Memento/Program.cs
+++ Memento/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Memento.Models;
 
 namespace Memento
 {
@@ -10,6 +11,31 @@ namespace Memento
     {
         static void Main(string[] args)
         {
+            Originator originator = new Originator("Inicio");
+            Caretaker caretaker = new Caretaker(originator);
+
+            caretaker.Backup();
+            originator.SetState("Estado 1");
+
+            caretaker.Backup();
+            originator.SetState("Estado 2");
+
+            caretaker.Backup();
+            originator.SetState("Estado 3");
+
+            Console.WriteLine();
+            caretaker.ShowHistory();
+
+            Console.WriteLine();
+            var x = originator.GetState();
+
+            Console.WriteLine("\nClient: Now, let's rollback!\n");
+            caretaker.Undo();
+
+            Console.WriteLine("\nClient: Now, let's second rollback!\n");
+            caretaker.Undo();
+
+            Console.ReadKey();
         }
     }
 }
